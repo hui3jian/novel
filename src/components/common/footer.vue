25cd6327e0614239209edef66fc5a3a5b5c8c2e9
Jordan zy <365129730@qq.com>
2018-03-15 15:43:59 +0800

    2018/3/15

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -3,10 +3,26 @@
 	<div class="footer">
     <mu-paper style="height: 100%">
       <mu-bottom-nav :value="bottomNav" shift @change="handleChange">
-        <mu-bottom-nav-item value="movies" title="书架" icon="local_library"/>
-        <mu-bottom-nav-item value="music" title="推荐" icon="thumb_up"/>
-        <mu-bottom-nav-item value="books" title="书圈" icon="stars"/>
-        <mu-bottom-nav-item value="pictures" title="用户" icon="person"/>
+        <mu-bottom-nav-item
+        value="movies"
+        to='/home'
+        title="书架"
+        icon="local_library"/>
+        <mu-bottom-nav-item
+        value="music"
+        to="/recommend"
+        title="推荐"
+        icon="thumb_up"/>
+        <mu-bottom-nav-item
+        value="books"
+        to="/square"
+        title="书圈"
+        icon="stars"/>
+        <mu-bottom-nav-item
+        value="pictures"
+        to="/user"
+        title="用户"
+        icon="person"/>
       </mu-bottom-nav>
     </mu-paper>
 	</div>
@@ -19,18 +35,18 @@
       return {
         bottomNav: 'movies',
         bottomNavColor: 'movies',
-        homeOpen:true,
-        recommendOpen:false,
-        squareOpen:false,
-        userOpen:false,
+        // homeOpen:false,
+        // recommendOpen:false,
+        // squareOpen:false,
+        // userOpen:false,
       }
     },
     mounted(){
-      var nowUrl = this.$route.path;
-      if(nowUrl.indexOf('home') >= 0) this.homeOpen = true;
-      if(nowUrl.indexOf('recommend') >= 0) this.homeOpen = true;
-      if(nowUrl.indexOf('square') >= 0) this.homeOpen = true;
-      if(nowUrl.indexOf('user') >= 0) this.homeOpen = true;
+      // var nowUrl = this.$route.path;
+      // if(nowUrl.indexOf('home') >= 0) this.homeOpen = true;
+      // if(nowUrl.indexOf('recommend') >= 0) this.recommendOpen = true;
+      // if(nowUrl.indexOf('square') >= 0) this.squareOpen = true;
+      // if(nowUrl.indexOf('user') >= 0) this.userOpen = true;
     },
     methods: {
       handleChange (val) {
